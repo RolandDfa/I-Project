42895cb78e67b2f3e9cc8c916fcec80ef9533366
InDeWater <dominicawater@gmail.com>
2019-05-08 14:53:11 +0200

    Login pagina expended

--- EenmaalAndermaal/content/login.php
+++ EenmaalAndermaal/content/login.php
@@ -1,36 +1,41 @@
 <div class="container login-container">
   <div class="row">
     <div class="col-md-6 login-form-1">
-      <h3>Login</h3>
+      <h3>Inloggen</h3>
+      <form action="" method="post">
+        <div class="form-group">
+          <input type="email" class="form-control" placeholder="Email *" value="" />
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Wachtwoord *" value="" />
+        </div>
+        <div class="form-group">
+          <input type="checkbox" name="blijfingelogd" value="ja"> Ingelogd blijven
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btnSubmit" value="Log in" />
+        </div>
+      </form>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Your Email *" value="" />
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" placeholder="Your Password *" value="" />
-      </div>
-      <div class="form-group">
-        <input type="submit" class="btnSubmit" value="Login" />
-      </div>
-      <div class="form-group">
-        <a href="" class="btnForgetPwd">Forget Password?</a>
+        <a href="" class="btnForgetPwd">Wachtwoord vergeten?</a>
       </div>
     </div>
     <div class="col-md-6 login-form-2">
       <div class="login-logo">
         <i class="fas fa-user-tie"></i>
       </div>
-      <h3>Registreer</h3>
+      <h3>Registreren</h3>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Your Email *" value="" />
+        <input type="button" class="btnSubmit" value="Maak account aan" onclick="location.href='index.php?page=registreren';" />
+      </div>&nbsp
+      <div class="form-group">
+        <i class="fas fa-user-tie"></i> Voordeel 1
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" placeholder="Your Password *" value="" />
+        <i class="fas fa-user-tie"></i> Voordeel 2
       </div>
       <div class="form-group">
-        <input type="submit" class="btnSubmit" value="Login" />
-      </div>
-      <div class="form-group">
-        Registreer <a href="index.php?page=registreren">hier</a>.
+        <i class="fas fa-user-tie"></i> Voordeel 3
       </div>
     </div>
   </div>
