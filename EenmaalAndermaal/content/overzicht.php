ba8a33a2b3c55043c978fe4fccefe06397120394
jurgverhoeven <jurgmill@gmail.com>
2019-05-13 15:13:02 +0200

    Overzicht pagina

---
+++ EenmaalAndermaal/content/overzicht.php
@@ -0,0 +1,216 @@
+<?php
+  if(isset($_POST['searchText'])){
+    $searchText = cleanInput($_POST['searchText']);
+  }
+  echo "Ingevoerde tekst: "."'".$searchText."'";
+?>
+
+
+<div class="pageWrapper">
+  <div class="row contentWrapper">
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/stoel.jpg" width="100%" height="220" alt="Stoel">
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Goeie Stoel (grondig getest)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/fiets.jpg" width="100%" height="220" alt="Fiets">
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Nieuwe fiets (bijna nieuw)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/klok.jpg" width="100%" height="220" alt="Klok">
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Antieke klok (originele bel)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="cardItem">
+      <a href="">
+        <div class="card shadow-sm">
+          <div class="cardImage">
+            <img class="rounded-top" src="images/gitaar.jpg" width="100%" height="220" alt="Gitaar"></a>
+          </div>
+          <div class="cardTitle">
+            <div class="cardHeader">
+              Akoestische gitaar (incl. snaren)
+            </div>
+            <div class="cardFooter">
+              Sluit zaterdag vanaf 20:00
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
