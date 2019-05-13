eb29022f788504cde2bb8f6795d05852ac003172
InDeWater <dominicawater@gmail.com>
2019-05-13 14:23:13 +0200

    Lettertypes

--- EenmaalAndermaal/index.php
+++ EenmaalAndermaal/index.php
@@ -27,9 +27,13 @@ if (!empty($_GET['page'])) {
 	<!-- Icons -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
+	<!-- Font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 
+	<!-- Icon -->
 	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
