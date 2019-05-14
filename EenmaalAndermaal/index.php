039ea4d9be4759930f49a02e18e0bb5b313e5b57
jurgverhoeven <jurgmill@gmail.com>
2019-05-14 12:15:46 +0200

    Overzicht

--- EenmaalAndermaal/index.php
+++ EenmaalAndermaal/index.php
@@ -109,7 +109,7 @@ if (!empty($_GET['page'])) {
 					<div class="col-lg-4">
 						<h5>Veilingen</h5>
 						<ul>
-							<li><a class="linkFooter" href="index.php?page=home">Alle veilingen</a></li>
+							<li><a class="linkFooter" href="index.php?page=overzicht">Alle veilingen</a></li>
 							<li><a class="linkFooter" href="index.php?page=home">Populair</a></li>
 							<li><a class="linkFooter" href="index.php?page=home">Zoeken</a></li>
 						</ul>
