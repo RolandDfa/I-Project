af4aaf70a70c48830fda345bb8774945590b47ef
jurgverhoeven <jurgmill@gmail.com>
2019-05-21 14:17:36 +0200

    See changes

--- EenmaalAndermaal/Databatch/clean.php
+++ EenmaalAndermaal/Databatch/clean.php
@@ -8,11 +8,16 @@ $dirtyStmt->execute();
 if($dirtyStmt->rowCount()!=0){
   $dirtyRows = $dirtyStmt->fetchAll();
   foreach ($dirtyRows as $dirtyRow) {
+    echo '<h1> Dirty: '.$dirtyRow['titel'].'</h2>';
+    echo '<p> Dirty: '.$dirtyRow['beschrijving'].'</p>';
+    $cleanTitle = cleanInput(strip_html_tags(replaceWhitespace($dirtyRow['titel'])));
+    $cleanDescription =  cleanInput(strip_html_tags(replaceWhitespace($dirtyRow['beschrijving'])));
+    echo '<h1> Clean: '.$cleanTitle.'</h1>';
+    echo '<p> Clean: '.$cleanDescription.'</p>';
+    echo '<br><br><br><br><br><br><br><br><br><br><br>';
     $cleanDataQuery = "UPDATE voorwerp SET titel = ?, beschrijving = ? where voorwerpnummer = ?";
     $cleanStmt = $dbh->prepare($cleanDataQuery);
     $cleanStmt->execute(array(cleanInput(strip_html_tags(replaceWhitespace($dirtyRow['titel']))), cleanInput(strip_html_tags(replaceWhitespace($dirtyRow['beschrijving']))), $dirtyRow['voorwerpnummer']));
-    echo '<h1>'.$dirtyRow['titel'].'</h2>';
-    echo '<p>'.$dirtyRow['beschrijving'].'</p>';
   }
 }
 ?>
