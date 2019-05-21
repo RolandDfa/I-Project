f198452e03ff77393dfb7623216e6cb970db5488
KlaasvdAkker <49900452+KlaasvdAkker@users.noreply.github.com>
2019-05-21 13:36:53 +0200

    Update privacybeleid.php

--- EenmaalAndermaal/content/privacybeleid.php
+++ EenmaalAndermaal/content/privacybeleid.php
@@ -1,5 +1,5 @@
 <div class="pageWrapper">
-  <h3 class="textCenter" class="greeneryText">
+  <br><h3 class="textCenter greeneryText">
    EENMAALANDERMAAL  PRIVACYVERKLARING  
   </h3>
   <br>
@@ -16,16 +16,44 @@
   <p>
     We gebruiken en verwerken de gegevens die we van u ontvangen. Bijvoorbeeld als een gebruiker zich registreert of als een gebruiker contact opneemt met onze klantenservice.
   </p><br>
-  <p><b>Account informatie. </b> Zodra u gebruik wilt maken van ons online platform (verkopen en kopen), dient u een account aan te maken. Dit doet u door een wachtwoord te kiezen en uw e-mailadres in te voeren. U kunt deze gegevens beheren via de accountinformatiepagina.
+  <p><b><i>Account informatie. </i></b> Zodra u gebruik wilt maken van ons online platform (verkopen en kopen), dient u een account aan te maken. Dit doet u door een wachtwoord te kiezen en uw e-mailadres in te voeren. U kunt deze gegevens beheren via de accountinformatiepagina.
   </p><br>
-  <p><b>Profiel informatie. </b> Om het mogelijk te maken voor ons om onze diensten aan te bieden en voor u om gebruik te maken van onze diensten, zullen wij u vragen om bepaalde informatie te verstrekken, zoals uw volledige naam, e-mailadres, telefoonnummer, adres, geboortedatum en het Kamer van Koophandel nummer van uw bedrijf.
+  <p><b><i>Profiel informatie. </i></b> Om het mogelijk te maken voor ons om onze diensten aan te bieden en voor u om gebruik te maken van onze diensten, zullen wij u vragen om bepaalde informatie te verstrekken, zoals uw volledige naam, e-mailadres, telefoonnummer, adres, geboortedatum en het Kamer van Koophandel nummer van uw bedrijf.
   </p><br>
-  <p><b>Communicatie informatie. </b> Wanneer u contact opneemt met onze klantenservice of wanneer zij met u contact opnemen, verzamelen wij informatie over de reden van dit contact en alle informatie die u aan ons verstrekt via de telefoon of e-mail. Dit omvat ook alle meldingen die we van anderen ontvangen over u of de door u aangeboden of gekochte kavels. Via ons online platform kunt u ook communiceren met andere gebruikers. We kunnen dergelijke communicatie beoordelen, scannen of analyseren voor fraudepreventie, risicobeoordeling, onderzoek en klantenservice doeleinden.
+  <p><b><i>Communicatie informatie. </i></b> Wanneer u contact opneemt met onze klantenservice of wanneer zij met u contact opnemen, verzamelen wij informatie over de reden van dit contact en alle informatie die u aan ons verstrekt via de telefoon of e-mail. Dit omvat ook alle meldingen die we van anderen ontvangen over u of de door u aangeboden of gekochte kavels. Via ons online platform kunt u ook communiceren met andere gebruikers. We kunnen dergelijke communicatie beoordelen, scannen of analyseren voor fraudepreventie, risicobeoordeling, onderzoek en klantenservice doeleinden.
+  </p><br><br>
+  <p><b>
+    Informatie die we automatisch verzamelen.
+    </b></p><br>
+  <p>
+    Wanneer u van ons Online Platform gebruikmaakt, verzamelen we informatie over uw activiteiten, zoals uw klikgedrag, geplaatste biedingen en informatie over uw apparaat om u de best mogelijke service te bieden.
+  </p><br>
+  <p><b><i>Gebruiksinformatie. </i></b> We houden interesseprofielen, kwalificaties en kenmerken van gebruikers bij, zodat we weten wat voor soort gebruiker u bent, uw locatie en welke kavels u leuk vindt. Dergelijke informatie stelt ons in staat om te ontdekken wat u interesseert en om onze dienst voor u te personaliseren. 
   </p><br><br>
   
-  
-  
-  
+  <h4 class="greeneryText"><b>
+    Voor welke doeleinden gebruikt EenmaalAndermaal deze informatie?
+    </b></h4><br>
+  <p> Hieronder kunt u lezen voor welke doeleinden de verzamelde informatie wordt gebruikt.
+  </p><br><br>
+  <p><b><i>De levering van onze diensten. </i></b></p><br>
+  <p> Wij gebruiken uw gegevens ten eerste om aan onze contractuele verplichtingen tegenover u te voldoen en om ervoor te zorgen dat onze dienst op de best mogelijke manier aan u wordt geboden. In dit verband gebruiken we uw gegevens om:
+  </p><br>
+  <ul>
+    <li>
+      U in staat te stellen om van ons Online Platform gebruik te maken.
+    </li>
+    <li>
+      Het Online Platform in een formaat te tonen dat overeenkomt met uw internetbrowser en het apparaat dat u gebruikt.
+    </li>
+    <li>
+        Klantenservice te bieden via verschillende kanalen, zoals e-mail en telefoon.
+    </li>
+    <li>
+      Onze interne administratie op orde te houden.
+    </li>
+  </ul>
+    
   <ul>
     <li>
       <i>Niet meer nodig</i><br>
