4130041181b1962405d5a1a7ea2e72dc6095e60b
KlaasvdAkker <49900452+KlaasvdAkker@users.noreply.github.com>
2019-05-22 10:59:15 +0200

    Update registrerenSucces.php

--- EenmaalAndermaal/content/registrerenSucces.php
+++ EenmaalAndermaal/content/registrerenSucces.php
@@ -1,2 +1,3 @@
 <?PHP
+echo '<img src="../../images/check.png" alt="registratie succesvol" height="100" width="100">';
 echo '<p>U bent succesvol geregistreerd, klik <a href="index.php?page=inloggen">Hier</a> om in te loggen<p>';
