1091a05a53bd7ab70707f8dad951ef6c6311ed3f
jurgverhoeven <jurgmill@gmail.com>
2019-05-22 11:28:49 +0200

    Added pagewrapper

--- EenmaalAndermaal/content/registrerenSucces.php
+++ EenmaalAndermaal/content/registrerenSucces.php
@@ -1,8 +1,9 @@
+<div class="pageWrapper">
+
 <?PHP
 echo '<img src="images/check.png" alt="registratie succesvol" height="100" width="100">';
 echo '<p>U bent succesvol geregistreerd!</p>';
 echo '<p> Klik <a href="index.php?page=inloggen">Hier</a> om naar uw accountinstellingen te gaan, of begin met <a href="overzicht.php">bieden</a>.</p>';
+?>
 
-
-
-
+</div>
